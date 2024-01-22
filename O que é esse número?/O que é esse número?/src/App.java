67ee8ba68185df25d799d57ab3e3f7ca440331f2
GabrielIGNS <gabrielgranjeia@alunos.utfpr.edu.br>
2024-01-22 16:43:47 -0300

    finished

---
+++ O que é esse número?/O que é esse número?/src/App.java
@@ -0,0 +1,8 @@
+import java.util.Random;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Random rand = new Random();
+        System.out.println(rand);
+    }
+}
